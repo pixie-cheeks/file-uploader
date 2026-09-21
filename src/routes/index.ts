ae9ab7341f2e4aa6376f6a2fdd0f4d5e30e38804
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2026-09-21 21:01:20 +0530

    Add multer to project

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,19 @@
 import type { Express } from 'express';
 import { NotFoundError } from '../middleware/errors.ts';
+import { upload } from '../middleware/multer.ts';
 
 export const setupRoutes = (app: Express): void => {
+  app.post('/', upload.single('avatar'), (request, response) => {
+    console.log({ file: request.file, body: request.body as object });
+    response.redirect('/');
+  });
+
   app.get('/', (_request, response) => {
     response.render('index', { title: 'home' });
   });
-  app.get('/*all', (_request, _response, next) => {
+
+  app.get('/*all', (request, _response, next) => {
+    console.log('params:', request.params.all);
     next(new NotFoundError('Page not found'));
   });
 };
